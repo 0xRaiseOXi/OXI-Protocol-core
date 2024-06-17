a1b954be8787c12a792a72de8731899485de1cec
0xRaiseOXi <maximusnesnescafe@mail.ru>
2024-06-17 11:41:56 +0300

    v11.8

--- src/main.rs
+++ src/main.rs
@@ -454,7 +454,7 @@ async fn main() -> std::io::Result<()> {
             .route("/newaccount", web::post().to(create_new_account))
             .service(actix_files::Files::new("/static", "./static").show_files_listing())
     })
-    .bind(("0.0.0.0", 8080))?
+    .bind(("127.0.0.1", 8080))?
     .run()
     .await
 }
